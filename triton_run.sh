07e04d2c49f8ecb100f370553812659841a5b0c9
kojuwonresearch <datapro@sidewalkplay.com>
2024-10-17 02:12:50 +0000

    echo test

--- triton_run.sh
+++ triton_run.sh
@@ -1,2 +1,3 @@
 docker build -t triton_vitpose .
+echo docker run --rm --gpus all -p 8000:8000 -p 8001:8001 -p 8002:8002 -v $pwd/pose_model_zoo:/models triton_vitpose tritonserver --model-repository=/models
 docker run --rm --gpus all -p 8000:8000 -p 8001:8001 -p 8002:8002 -v $pwd/pose_model_zoo:/models triton_vitpose tritonserver --model-repository=/models
